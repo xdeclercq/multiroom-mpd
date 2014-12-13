67182310c8b389a87dca5f5208d9142ced7312a6
Xavier Declercq <xavier_declercq@yahoo.fr>
2014-12-13 23:06:33 +0100

    Added test Updated license to MIT

--- src/test/java/com/autelhome/multiroom/app/MultiroomMPDConfigurationTest.java
+++ src/test/java/com/autelhome/multiroom/app/MultiroomMPDConfigurationTest.java
@@ -2,7 +2,14 @@ package com.autelhome.multiroom.app;
 
 import com.autelhome.multiroom.zone.ZoneConfiguration;
 import com.autelhome.multiroom.zone.ZonesConfiguration;
+import com.google.common.io.Resources;
+import io.dropwizard.configuration.ConfigurationFactory;
+import io.dropwizard.jackson.Jackson;
 import org.junit.Test;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.io.File;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -20,5 +27,23 @@ public class MultiroomMPDConfigurationTest {
         ZonesConfiguration actual = configuration.getZonesConfiguration();
 
         assertThat(actual, equalTo(expected));
+    }
+
+
+    @Test
+    public void deserializeFromYAML() throws Exception {
+        ZonesConfiguration expected = new ZonesConfiguration();
+        expected.add(new ZoneConfiguration("Kitchen", 6600));
+        expected.add(new ZoneConfiguration("Bathroom", 6601));
+
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        final ConfigurationFactory<MultiroomMPDConfiguration> factory =
+                new ConfigurationFactory<>(MultiroomMPDConfiguration.class, validator, Jackson.newObjectMapper(), "dw");
+
+
+        MultiroomMPDConfiguration actual = factory.build(new File(Resources.getResource("fixtures/configuration.yml").toURI()));
+
+        assertThat(actual.getZonesConfiguration(), equalTo(expected));
     }
 }
