a457ed82c289c83517b4cd72492b2e1e96d6e226
Xavier Declercq <xavier_declercq@yahoo.fr>
2014-12-16 23:34:14 +0100

    Updated Guice module to provide zones configuration (#1)

--- src/main/java/com/autelhome/multiroom/app/MultiroomMPDModule.java
+++ src/main/java/com/autelhome/multiroom/app/MultiroomMPDModule.java
@@ -1,6 +1,8 @@
 package com.autelhome.multiroom.app;
 
+import com.autelhome.multiroom.zone.ZonesConfiguration;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 
 /**
  * Guice module.
@@ -12,5 +14,16 @@ public class MultiroomMPDModule extends AbstractModule {
     @Override
     protected void configure() {
         // do nothing
+    }
+
+    /**
+     * Returns the zones configuration.
+     *
+     * @param configuration the application configuration
+     * @return the zones configuration
+     */
+    @Provides
+    public ZonesConfiguration getZonesConfiguration(MultiroomMPDConfiguration configuration) {
+        return configuration.getZonesConfiguration();
     }
 }
