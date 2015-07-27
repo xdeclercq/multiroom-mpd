e8cc63818e74ae9bb96c250ac6012b4295fd9d9f
Xavier Declercq <xavier.declercq@yahoo.fr>
2015-07-27 13:28:54 +0200

    Defined CurrentSongUpdated event and related unit tests (#17)

---
+++ src/main/java/com/autelhome/multiroom/player/CurrentSongUpdated.java
@@ -0,0 +1,64 @@
+package com.autelhome.multiroom.player;
+
+import com.autelhome.multiroom.song.Song;
+import com.autelhome.multiroom.util.AbstractEvent;
+import com.google.common.base.MoreObjects;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Event triggered when the current song of a player has been updated.
+ *
+ * @author xdeclercq
+ */
+public class CurrentSongUpdated extends AbstractEvent {
+
+    private final Song newCurrentSong;
+
+    /**
+     * Constructor.
+     *
+     * @param zoneId the zone id
+     * @param newCurrentSong then new current song
+     */
+    public CurrentSongUpdated(final UUID zoneId, final Song newCurrentSong) {
+        super(zoneId);
+        this.newCurrentSong = newCurrentSong;
+    }
+
+    public Song getNewCurrentSong() {
+        return newCurrentSong;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final CurrentSongUpdated that = (CurrentSongUpdated) o;
+
+        if (id != that.id) {
+            return false;
+        }
+
+        return Objects.equals(newCurrentSong, that.newCurrentSong);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, newCurrentSong);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("newCurrentSong", newCurrentSong)
+                .toString();
+    }
+}
---
+++ src/test/java/com/autelhome/multiroom/player/CurrentSongUpdatedTest.java
@@ -0,0 +1,74 @@
+package com.autelhome.multiroom.player;
+
+import com.autelhome.multiroom.song.Song;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class CurrentSongUpdatedTest {
+
+    private static final UUID ZONE_ID = UUID.randomUUID();
+    private static final String SONG_A = "Song A";
+    private static final String SONG_B = "Song B";
+
+    @Test
+    public void getNewCurrentSong() throws Exception {
+        final Song expected = new Song(SONG_A);
+        final Song actual = new CurrentSongUpdated(UUID.randomUUID(), expected).getNewCurrentSong();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldBeEqual() throws Exception {
+        final CurrentSongUpdated currentSongUpdated1 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_A));
+        final CurrentSongUpdated currentSongUpdated2 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_A));
+
+        assertThat(currentSongUpdated1).isEqualTo(currentSongUpdated1);
+        assertThat(currentSongUpdated1).isEqualTo(currentSongUpdated2);
+    }
+
+    @Test
+    public void shouldNotBeEqual() throws Exception {
+        final CurrentSongUpdated currentSongUpdated1 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_A));
+        final CurrentSongUpdated currentSongUpdated2 = new CurrentSongUpdated(UUID.randomUUID(), new Song(SONG_A));
+        final CurrentSongUpdated currentSongUpdated3 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_B));
+
+        assertThat(currentSongUpdated1).isNotEqualTo(" ");
+        assertThat(currentSongUpdated1).isNotEqualTo(null);
+        assertThat(currentSongUpdated1).isNotEqualTo(currentSongUpdated2);
+        assertThat(currentSongUpdated1).isNotEqualTo(currentSongUpdated3);
+    }
+
+    @Test
+    public void hashCodeShouldBeTheSame() throws Exception {
+        final int hashCode1 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_A)).hashCode();
+        final int hashCode2 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_A)).hashCode();
+
+        assertThat(hashCode1).isEqualTo(hashCode2);
+    }
+
+    @Test
+    public void hashCodeShouldNotBeTheSame() throws Exception {
+        final int hashCode1 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_A)).hashCode();
+        final int hashCode2 = new CurrentSongUpdated(UUID.randomUUID(), new Song(SONG_A)).hashCode();
+        final int hashCode3 = new CurrentSongUpdated(ZONE_ID, new Song(SONG_B)).hashCode();
+
+        assertThat(hashCode1).isNotEqualTo(hashCode2);
+        assertThat(hashCode1).isNotEqualTo(hashCode3);
+    }
+
+    @Test
+    public void toStringShouldContainFieldsValue() throws Exception {
+        final UUID id = UUID.randomUUID();
+        final Song newCurrentSong = new Song(SONG_A);
+        final String actual = new CurrentSongUpdated(id, newCurrentSong).toString();
+
+        MatcherAssert.assertThat(actual, containsString(id.toString()));
+        MatcherAssert.assertThat(actual, containsString(newCurrentSong.toString()));
+    }
+}
